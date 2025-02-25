b0ee7d35c83b12b0fdd11070f1410c52a50f4b27
vivdychenko.maxim2006@gmail.com <vivdychenko.maxim2006@gmail.com>
2025-02-25 16:16:51 +0200

    made page for projects and project

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.Projects, name='projects')
+    path('', views.Projects, name='projects'),
+    path('project/<str:id>/', views.Single_proj, name='single_project'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
+project_list = [{'id': '1', 'title': 'Booking service', 'description' : 'Site for booking every object that you need'},
+                {'id': '2', 'title': 'Cinema service', 'description' : 'Site for watching movies'},
+                {'id': '3', 'title': 'OLX service', 'description' : 'Sell and buy site'}]
+
 def Projects(request):
-    return render(request,'projects/projects.html')
+    return render(request,'projects/projects.html', {'projects': project_list})
+
+def Single_proj(request, id):
+    project = None
+    for i in project_list:
+        if i['id'] == id:
+            project = i['id']
+            break
+    return render(request,'projects/single-project.html', {'project': project})
