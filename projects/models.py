8ba398b4aa991474eab2419f0fa3011738bd50fc
vivdychenko.maxim2006@gmail.com <vivdychenko.maxim2006@gmail.com>
2025-02-26 11:11:41 +0200

    added project model

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,13 @@
 from django.db import models
+import uuid
 
 # Create your models here.
 class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.URLField(max_length=2000, null=True, blank=True)
+    source_link = models.URLField(max_length=2000, null=True, blank=True)
+    date_created = models.DateField(auto_now_add=True)
+    def __str__(self):
+        return self.title
